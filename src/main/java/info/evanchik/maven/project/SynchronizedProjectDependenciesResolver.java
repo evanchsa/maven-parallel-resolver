5a3a7718f03b4e2a459f5b88842d19fc55fc98e3
Stephen Evanchik <evanchsa@gmail.com>
2013-10-11 20:24:33 -0400

    maven: Replacement component for concurrent safe parallel builds
     - DefaultProjectDependenciesResolver has a synchronization problem
       which makes parallel builds fail when an artifact is resolved by two
       or more threads.
     - Consult http://jira.codehaus.org/browse/MNG-5315

---
+++ src/main/java/info/evanchik/maven/project/SynchronizedProjectDependenciesResolver.java
@@ -0,0 +1,47 @@
+package info.evanchik.maven.project;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.project.DefaultProjectDependenciesResolver;
+import org.apache.maven.project.DependencyResolutionException;
+import org.apache.maven.project.DependencyResolutionRequest;
+import org.apache.maven.project.DependencyResolutionResult;
+import org.apache.maven.project.ProjectDependenciesResolver;
+
+import org.codehaus.plexus.component.annotations.Component;
+
+/**
+ * @author Stephen Evanchik
+ */
+@Component( role = ProjectDependenciesResolver.class )
+public class SynchronizedProjectDependenciesResolver extends DefaultProjectDependenciesResolver
+{
+
+    private Object lock = new Object();
+
+    public DependencyResolutionResult resolve( DependencyResolutionRequest request )
+        throws DependencyResolutionException
+    {
+        synchronized (lock)
+        {
+            return super.resolve( request );
+        }
+    }
+}
